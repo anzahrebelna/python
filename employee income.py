d34b40b3210688e0f9a269a90f3a251d1d818a62
anzahrebelna <94645502+anzahrebelna@users.noreply.github.com>
2022-03-15 20:25:18 +0100

    Update and rename 3.py to employee income.py
    Create a text file (not programmatically), write down the names of employees and their salaries line by line. Determine which of the employees has a salary of less than 20 thousand, display the names of these employees. Calculate the average income of employees

--- employee income.py
+++ employee income.py
@@ -7,4 +7,4 @@ with open('task3.txt', 'r') as my_file:
         if float(i[1]) < 20000:
             poor.append(i[0])
         salary.append(i[1])
-    print(f'Оклад меньше 20.000 {poor}, средний оклад {sum(map(float, salary)) / len(salary)}')
+    print(f'Salary less than 20.000 {poor}, average salary {sum(map(float, salary)) / len(salary)}')
